710f7b1b9a47baad3bdf3e58a135d96c1acb2fb7
artur <aartur.now@gmail.com>
2025-02-02 13:12:50 +0100

    Init

---
+++ database.py
@@ -0,0 +1,45 @@
+from sqlalchemy import create_engine, Column, Text, Integer, DateTime, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from contextlib import contextmanager
+
+engine = create_engine(
+    "sqlite:///./test.db", connect_args={"check_same_thread": False}
+)
+
+Base = declarative_base()
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+@contextmanager
+def get_session():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def get_session_fa():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+users = [
+    {
+        "login": "admin",
+        "password": "admin",
+        "token": 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjoiYWRtaW4iLCJleHAiOjE3Mzg1MzY3NzB9.t7hleSIU128HWkalUBMSrETlk051SFtksGCzfgITQ58'
+    }
+]
+
+class Message(Base):
+    __tablename__ = 'messages'
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    send_time = Column(DateTime)
+    sender = Column(String(50), nullable=False)
+    message = Column(Text)
+
+Base.metadata.create_all(bind=engine)
